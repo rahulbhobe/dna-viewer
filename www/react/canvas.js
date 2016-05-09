fb94394e1833a8eb847222b77b640b16e72c3f90
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-05-09 06:56:47 -0400

    Some vector math

--- www/react/canvas.js
+++ www/react/canvas.js
@@ -9,6 +9,13 @@ var Canvas = React.createClass({
     var subStructure = sequenceParser.getSubStructureAtIndex(base0.getIndex());
     var numNodes = subStructure.getNodes().length;
 
+    var red    = Vector.create([800, 100]);
+    var vec    = Vector.create([0, -1]);
+    var green  = red.add(vec.multiply(50));
+    var blue   = green.rotate(Math.PI/4, red);
+    var yellow = green.rotate((3*Math.PI)/4, red);
+    var orange = green.rotate((5*Math.PI)/4, red);
+
     return (
       <svg width='1500' height='900'>
         <g transform="translate(500, 100)">
@@ -26,6 +33,23 @@ var Canvas = React.createClass({
             </g>
           </g>
         </g>
+
+        <g transform={"translate(" + red.elements[0] + ", " + red.elements[1] + ")"}>
+          <circle cx={0} cy={0} r={8} fill="red" />
+        </g>
+        <g transform={"translate(" + green.elements[0] + ", " + green.elements[1] + ")"}>
+          <circle cx={0} cy={0} r={8} fill="green" />
+        </g>
+        <g transform={"translate(" + blue.elements[0] + ", " + blue.elements[1] + ")"}>
+          <circle cx={0} cy={0} r={8} fill="blue" />
+        </g>
+        <g transform={"translate(" + yellow.elements[0] + ", " + yellow.elements[1] + ")"}>
+          <circle cx={0} cy={0} r={8} fill="yellow" />
+        </g>
+        <g transform={"translate(" + orange.elements[0] + ", " + orange.elements[1] + ")"}>
+          <circle cx={0} cy={0} r={8} fill="orange" />
+        </g>
+
       </svg>);
   }
 });
