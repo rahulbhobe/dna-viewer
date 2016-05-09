5f905ebcafcfb013a5e3ba8540f5a93614e4fb9c
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-05-09 19:24:16 -0400

    More cleanup

--- www/src/geometry_solver.js
+++ www/src/geometry_solver.js
@@ -16,15 +16,15 @@ var GeometrySolver = function (sequenceParser) {
 
   var centerPosition = Vector.create([700, 400]); // Start position for the center.
 
-  var prevPoint = null;
-  var prevBase = null;
-  var prevTheta = null;
+  var moveCenter = false;
+  var prevPoint  = null;
+  var prevTheta  = null;
   var coordinates = [];
   _(sequenceParser.getBases()).each(function (base) {
     var thisTheta = getThetaFromBase(base);
     prevPoint = prevPoint || centerPosition.add(Vector.create([0, -1]).multiply(getRadiusFromTheta(thisTheta)));
 
-    if (prevBase && !prevBase.isUnpaired()) {
+    if (moveCenter) {
       var kkk = getDistanceToMoveCenter(thisTheta) + getDistanceToMoveCenter(prevTheta);
       var point = prevPoint.rotate(prevTheta/2, centerPosition);
       var vec   = point.subtract(centerPosition).toUnitVector().multiply(kkk);
@@ -34,8 +34,8 @@ var GeometrySolver = function (sequenceParser) {
     var thisPoint = prevPoint.rotate(thisTheta, centerPosition);
     coordinates.push(thisPoint);
     prevPoint = thisPoint; // For next iteration.
-    prevBase = base;
     prevTheta = thisTheta;
+    moveCenter = !base.isUnpaired();
   });
 
   return {
