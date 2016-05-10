0c7976c3f48f8a541fece3b48a1ea259a8cf2921
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-05-10 09:31:44 -0400

    Added text

--- www/react/canvas.js
+++ www/react/canvas.js
@@ -1,21 +1,27 @@
 
+var DnaBaseView = React.createClass({
+  render: function () {
+    var point = this.props.point;
+    var base  = this.props.base;
+    return (<g transform={"translate(" + point.elements[0] + ", " + point.elements[1] + ")"}>
+              <circle cx={0} cy={0} r={20} fill="red" />
+              <text x={-4} y={4} > {base.getType()}</text>
+            </g>);
+  }
+});
+
 
 var Canvas = React.createClass({
   render: function () {
     var sequenceParser = this.props.sequenceParser;
-
-
     var coordinates = sequenceParser.getCoordinates();
-
-    var circles = _(coordinates).map(function (point) {
-      return (<g transform={"translate(" + point.elements[0] + ", " + point.elements[1] + ")"}>
-                <circle cx={0} cy={0} r={5} fill="red" />
-              </g>);
-    });
+    var bases       = sequenceParser.getBases();
 
     return (
       <svg width='1500' height='900'>
-        {circles}
+        {_(coordinates).map(function (point, ii) {
+            return (<DnaBaseView point={point} base={bases[ii]}/>);
+          })}
       </svg>);
   }
 });
