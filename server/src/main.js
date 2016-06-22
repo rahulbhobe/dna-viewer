999eac75d94add9b59689564dc3d1474d562dbd5
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-06-22 05:32:53 -0400

    Used babel-cli instead of babel-register

--- server/src/main.js
+++ server/src/main.js
@@ -11,9 +11,9 @@ var Data        =  Promise.promisifyAll(DataImport);
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.use('/', express.static(__dirname + '/../www'));
-app.use(favicon(__dirname + '/../www/res/favicon.ico'));
-app.set('views', __dirname + '/../www');
+app.use('/', express.static(__dirname + '/../../www'));
+app.use(favicon(__dirname + '/../../www/res/favicon.ico'));
+app.set('views', __dirname + '/../../www');
 app.set('view engine', 'ejs');
 app.use(bodyParser.json());
 
