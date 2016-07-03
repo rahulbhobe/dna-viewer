df2251a0605b9a01342bd92e044800f647e4b191
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-07-03 10:23:42 -0400

    Used mongodb on cloud

--- server/src/db_manager.js
+++ server/src/db_manager.js
@@ -1,18 +1,25 @@
 import mongoose from 'mongoose';
 
 var DbManager = function() {
-  var mongoURI = 'mongodb://localhost/myappdatabase';
   var connected = false;
 
-  mongoose.connection.on('connected', function () {
+  var getMongoURI = function (includeCredentials) {
+    return 'mongodb://' + (includeCredentials ? 'dna_user:dna_password@' : '')
+                   + 'ds011755.mlab.com:11755/dna_viewer';
+  };
+
+  mongoose.connection.on('connected', function (aa, bb) {
     connected = true;
+    console.log('Connected to: ', getMongoURI(false));
   });
 
   mongoose.connection.on('reconnected', function () {
+    console.log('Reconnected to: ', getMongoURI(false));
     connected = true;
   });
 
   mongoose.connection.on('disconnected', function () {
+    console.log('Disconnected from: ', getMongoURI(false));
     connected = false;
   });
 
@@ -23,7 +30,7 @@ var DbManager = function() {
 
   return {
     init: function () {
-      mongoose.connect(mongoURI);
+      mongoose.connect(getMongoURI(true));
       mongoose.Promise = Promise;
     },
 
