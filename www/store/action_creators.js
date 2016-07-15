3ea75ec22f85b66fdf2f383bd9c9a9ebb7310770
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-07-15 03:08:12 -0400

    Added missing semi-colons.

--- www/store/action_creators.js
+++ www/store/action_creators.js
@@ -2,24 +2,24 @@ import * as ActionTypes from './action_types';
 
 export var hoverNodeSet = (node) => {
   return { type: ActionTypes.HOVER_NODE_SET, value: node };
-}
+};
 
 export var hoverNodeReset = () => {
   return { type: ActionTypes.HOVER_NODE_RESET };
-}
+};
 
 export var draggingNodeSet = (node) => {
   return { type: ActionTypes.DRAGGING_NODE_SET, value: node };
-}
+};
 
 export var draggingNodeReset = () => {
   return { type: ActionTypes.DRAGGING_NODE_RESET };
-}
+};
 
 export var mousePositionSet = (x, y) => {
   return { type: ActionTypes.MOUSE_POSITION_SET, value: {x, y} };
-}
+};
 
 export var mousePositionReset = () => {
   return { type: ActionTypes.MOUSE_POSITION_RESET };
-}
+};
