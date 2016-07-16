514797ccd5317086c1d80e58ed5d74332e4c1cfd
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-07-16 02:00:02 -0400

    Minor.

--- www/store/action_types.js
+++ www/store/action_types.js
@@ -1,8 +1,6 @@
-export const SET_HOVER_NODE       = 'SET_HOVER_NODE';
-export const RESET_HOVER_NODE     = 'RESET_HOVER_NODE';
-export const SET_DRAGGING_NODE    = 'SET_DRAGGING_NODE';
-export const RESET_DRAGGING_NODE  = 'RESET_DRAGGING_NODE';
-export const SET_MOUSE_POSITION   = 'SET_MOUSE_POSITION';
-export const RESET_MOUSE_POSITION = 'RESET_MOUSE_POSITION';
-
-
+export const SET_HOVER_NODE           = 'SET_HOVER_NODE';
+export const RESET_HOVER_NODE         = 'RESET_HOVER_NODE';
+export const SET_DRAGGING_NODE        = 'SET_DRAGGING_NODE';
+export const RESET_DRAGGING_NODE      = 'RESET_DRAGGING_NODE';
+export const SET_MOUSE_POSITION       = 'SET_MOUSE_POSITION';
+export const RESET_MOUSE_POSITION     = 'RESET_MOUSE_POSITION';
