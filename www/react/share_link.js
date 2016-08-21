484cb47cfc19ea939c640893b8a08e8952402859
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-08-21 18:53:24 -0400

    Removed state (not needed).

--- www/react/share_link.js
+++ www/react/share_link.js
@@ -6,30 +6,18 @@ import {connect} from 'react-redux';
 class ShareLink extends React.Component {
   constructor (props) {
     super(props);
-    this.state = {
-      url: window.location.origin,
-      expanded: false
-    };
     this.onClick   = this.onClick.bind(this);
     this.onSuccess = this.onSuccess.bind(this);
     this.onError   = this.onError.bind(this);
   };
 
   render () {
-    var clsName = (this.state && this.state.expanded) ? "" : "share-link-box-hidden";
-    clsName += " share-links-box";
     return (<div className="share-link-button" >
               <input type="image" src="/res/share_icon.png" alt="Submit" onClick={this.onClick}/>
-              <span className={clsName}>{this.state.url}</span>
             </div>);
   };
 
   onClick () {
-    if (this.state.expanded) {
-      this.setState({expanded: false});
-      return;
-    }
-
     var data = {
       seq: this.props.seq,
       dbn: this.props.dbn
@@ -45,32 +33,10 @@ class ShareLink extends React.Component {
 
   onSuccess (data) {
     var url = window.location.origin + '/' + data.url;
-    this.setState({
-      url: url,
-      expanded: true,
-      seq: this.props.seq,
-      dbn: this.props.dbn
-    });
   };
 
   onError (err) {
     console.log('Server Error: ', err);
-  };
-
-  componentWillReceiveProps (nextProps) {
-    if (!this.state) return;
-    if (!this.state.expanded) return;
-
-    if ((this.state.seq===nextProps.seq) && (this.state.dbn===nextProps.dbn)) {
-      return;
-    }
-
-    this.setState({
-      url: null,
-      expanded: false,
-      seq: nextProps.seq,
-      dbn: nextProps.dbn
-    });
   };
 };
 
