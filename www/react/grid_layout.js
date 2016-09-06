4f49e4aa50b660e1e79d791a3ec7fffefddf502b
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-09-06 02:11:20 -0400

    Filtered only visible layouts to avoid passing items (divs) that are out of bounds.

--- www/react/grid_layout.js
+++ www/react/grid_layout.js
@@ -13,8 +13,14 @@ class GridLayout extends React.Component {
     return 'grid_layout_key_' + idx;
   };
 
+  getVisibleLayout () {
+    return this.props.layout.filter(({v}) => {
+      return v === true;
+    });
+  };
+
   getLayout () {
-    return this.props.layout.map(({x, y, w, h}, idx) => {
+    return this.getVisibleLayout().map(({x, y, w, h}, idx) => {
       var i = this.getKeyFromIndex(idx);
       return {i, x, y, w, h};
     });
@@ -32,7 +38,7 @@ class GridLayout extends React.Component {
 
     return (
       <ReactGridLayout layout={this.getLayout()} {...properties} >
-        {this.props.layout.map((item, idx) => {
+        {this.getVisibleLayout().map((item, idx) => {
           return (
             <div key={this.getKeyFromIndex(idx)} >
               {item.v ? item.d : null}
