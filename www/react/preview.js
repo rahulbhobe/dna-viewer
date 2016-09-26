37181befd2b7308207fb10025567b966ae0d9ec3
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-09-26 01:41:32 -0400

    Submitted first round for preview.

---
+++ www/react/preview.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import {Vector} from '../mathutils/gl_matrix_wrapper';
+import classNames from 'classnames';
+import {connect} from 'react-redux';
+
+class Preview extends React.Component {
+  render () {
+    var sequenceParser = this.props.sequenceParser;
+    if (sequenceParser.hasErrors()) {
+      return null;
+    }
+
+    var bases       = sequenceParser.getBases();
+    var width       = this.getWindowWidth();
+    var height      = this.getWindowHeight();
+    var coordinates = this.getCoordinatesForScreen();
+    var bases       = sequenceParser.getBases();
+
+    return (
+      <svg className='svg-class' width={width} height={height} ref='svg' onContextMenu={this.onContextMenu} >
+        {coordinates.map((point, ii) => {
+            var {x, y}  = point.asObj();
+            var base    = bases[ii];
+            var classes = classNames('dna-base-preview', 'dna-base-' + base.getType().toLowerCase());
+            return (<g transform={'translate(' + x + ', ' + y + ')'} key={'base_preview' + ii} >
+                      <circle className={classes} />
+                    </g>);
+        })}
+      </svg>);
+  };
+
+  getWindowWidth () {
+    return 235;
+  };
+
+  getWindowHeight () {
+    return 46 * 3;
+  };
+
+  getCoordinatesForScreen () {
+    var width          = this.getWindowWidth();
+    var height         = this.getWindowHeight();
+    var sequenceParser = this.props.sequenceParser;
+    return sequenceParser.getCoordinates(width, height);
+  };
+};
+
+var mapStateToProps = (state, ownProps) => {
+  return {
+    sequenceParser: state.sequenceParser
+  };
+};
+
+export default connect(mapStateToProps)(Preview);
