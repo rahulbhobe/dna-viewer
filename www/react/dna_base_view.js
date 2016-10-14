31e526d723204c9579bb0c90d4d67debf10bf110
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-10-14 17:34:11 -0400

    Added drag capability in DnaBaseView.

--- www/react/dna_base_view.js
+++ www/react/dna_base_view.js
@@ -1,8 +1,18 @@
 import React from 'react';
+import * as d3 from 'd3';
 import {connect} from 'react-redux';
 import classNames from 'classnames';
 
 class DnaBaseView extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.dragStarted  = this.dragStarted.bind(this);
+    this.dragProgress = this.dragProgress.bind(this);
+    this.dragEnded    = this.dragEnded.bind(this);
+  };
+
+
   render () {
     if (this.props.ignore) return null;
     let {x, y, index, type} = this.props;
@@ -12,10 +22,50 @@ class DnaBaseView extends React.Component {
     var textCls = classNames('dna-text', 'dna-base-font');
     var clsName = classNames({'dna-base-banned-pairing': this.props.bannedCursorWhenMoving});
 
-    return (<g className={clsName} transform={'translate(' + x + ', ' + y + ')'}>
+    return (<g className={clsName} ref='gnode' transform={'translate(' + x + ', ' + y + ')'}>
               <circle className={classes} data-index={index} />
               <text className={textCls} textAnchor='middle' dominantBaseline='central'>{type}</text>
             </g>);
+  };
+
+  componentDidMount () {
+    this.registerDragCallbacks();
+  };
+
+  componentDidUpdate () {
+    this.registerDragCallbacks();
+  };
+
+  registerDragCallbacks () {
+    let gnode = this.refs.gnode;
+    if (!gnode) return;
+
+    d3.select(gnode).call(d3.drag()
+                    .on('start', this.dragStarted)
+                    .on('drag', this.dragProgress)
+                    .on('end', this.dragEnded));
+  };
+
+  dragStarted () {
+    let simulation = this.props.simulation;
+    let node       = this.props.node;
+    if (!d3.event.active) simulation.alphaTarget(0.3).restart();
+    node.fx = node.x;
+    node.fy = node.y;
+  };
+
+  dragProgress () {
+    let node       = this.props.node;
+    node.fx = d3.event.x;
+    node.fy = d3.event.y;
+  };
+
+  dragEnded () {
+    let simulation = this.props.simulation;
+    let node       = this.props.node;
+    if (!d3.event.active) simulation.alphaTarget(0);
+    node.fx = null;
+    node.fy = null;
   };
 };
 
@@ -31,6 +81,8 @@ var mapStateToProps = (initialState, initialOwnProps) => {
       index: index,
       x: animated[index].x.toFixed(2),
       y: animated[index].y.toFixed(2),
+      node: animated[index],
+      simulation: state.simulatedData.simulation,
       type: bases[index].getType(),
       hover: state.hover === index,
       dragging: state.dragging === index
