d4b6da42d522ae2a0e87cb936b4dc26645fe431f
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-10-17 03:04:37 -0400

    Adjusted settings view dimensions.

--- www/react/settings_view.js
+++ www/react/settings_view.js
@@ -11,13 +11,18 @@ import GridLayout from './grid_layout';
 class SettingsView extends React.Component {
   getLayout () {
     let notPickingColor  = this.props.notPickingColor;
+    let th = this.props.canvasDimensions.height / Dimensions.DNA_VIEWER_ROW_HEIGHT;
+    let sv = th > 7 + 8;
+    let pv = th > 6 + 7 + 8;
+    let sh = pv ? th-13 : th-7;
+    let ph = th-6;
 
     return [
-      {x:0,  y:0,  w:2,  h:3,  v: true,             i: 'ColorSettings'},
-      {x:0,  y:3,  w:1,  h:5,  v: notPickingColor,  i: 'AppearanceSettings'},
-      {x:1,  y:3,  w:1,  h:2,  v: notPickingColor,  i: 'FontSettings'},
-      {x:0,  y:8,  w:2,  h:7,  v: notPickingColor,  i: 'SavedViews'},
-      {x:0,  y:15, w:2,  h:6,  v: notPickingColor,  i: 'Preview'}
+      {x:0,  y:0,   w:2,  h:3,  v: true,                  i: 'ColorSettings'},
+      {x:0,  y:3,   w:1,  h:5,  v: notPickingColor,       i: 'AppearanceSettings'},
+      {x:1,  y:3,   w:1,  h:2,  v: notPickingColor,       i: 'FontSettings'},
+      {x:0,  y:sh,  w:2,  h:7,  v: sv && notPickingColor, i: 'SavedViews'},
+      {x:0,  y:ph,  w:2,  h:6,  v: pv && notPickingColor, i: 'Preview'}
     ];
   };
 
@@ -42,7 +47,8 @@ class SettingsView extends React.Component {
 
 var mapStateToProps = (state, ownProps) => {
   return {
-    notPickingColor: state.pickingColor === null
+    notPickingColor: state.pickingColor === null,
+    canvasDimensions: state.canvasDimensions
   }
 };
 
