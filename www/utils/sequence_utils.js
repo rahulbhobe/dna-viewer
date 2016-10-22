10427b5d866cc8cf57bfc0e4845128418596cc5d
Rahul Bhobe <Rahul.Bhobe@autodesk.com>
2016-10-22 13:13:27 -0400

    Extracted the function SequenceUtils.canJoinNodes().

--- www/utils/sequence_utils.js
+++ www/utils/sequence_utils.js
@@ -1,19 +1,25 @@
 import SequenceParser from '../src/sequence_parser';
 
 class SequenceUtils {
+  static canJoinNodes (sequenceParser, node1, node2) {
+    if (node1 === -1)    return false;
+    if (node2 === -1)    return false;
+    if (node1 === node2) return false;
+
+    let bases = sequenceParser.getBases();
+    let base1 = bases[node1];
+    let base2 = bases[node2];
+
+    if (!base1.isUnpaired()) return false;
+    if (!base2.isUnpaired()) return false;
+
+    return base1.canPairWith(base2);
+  };
+
   static getJoinedSequence (sequenceParser, node1, node2) {
     if (!sequenceParser) return null;
-    if (node1 === -1)    return null;
-    if (node2 === -1)    return null;
-    if (node1 === node2) return null;
 
-    let bases    = sequenceParser.getBases();
-    let base1    = bases[node1];
-    let base2    = bases[node2];
-
-    if (!base1.isUnpaired()) return null;
-    if (!base2.isUnpaired()) return null;
-    if (!base1.canPairWith(base2)) return null;
+    if (!this.canJoinNodes(sequenceParser, node1, node2)) return null;
 
     let {seq, dbn} = sequenceParser.getData();
     let min = Math.min(node1, node2);
